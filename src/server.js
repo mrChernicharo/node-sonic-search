ad4579ddfd2eb701f9021a5abe77db002e7ab539
Felipe Chernicharo <string7dev@gmail.com>
2021-03-03 23:51:39 -0300

    192.168.99.100 -> conexão com container docker

--- src/server.js
+++ src/server.js
@@ -5,23 +5,45 @@ const { Ingest } = require("sonic-channel");
 const app = express();
 
 const sonicChannelIngest = new Ingest({
-  host: "localhost",
+  // host: "localhost", <- não funciona pois meu docker roda por default nesse TCP aqui 192.168.99.100
+  host: "192.168.99.100",
+  //port: 2376
   port: 1491,
-  auth: "superSecretPassword",
+  auth: "SecretPassword", // <- não adianta mudar lá no sonic.cfg, a senha tem q ser essa
 });
 
 sonicChannelIngest.connect({
   connected: () => {
     console.log("conectou");
   },
+  timeout: () => {
+    console.log("timeout");
+  },
+  error: (e) => {
+    console.warn("erro!");
+    console.warn(e);
+  },
+  disconnected: () => {
+    console.log("disconnected");
+  },
 });
 
-app.post("/pages", (req, res) => {
+app.post("/pages", async (req, res) => {
   const { title, content } = req.body;
   const id = uuid();
 
-  // cadastrar página no banco ...
+  console.log(title, content, id);
 
+  // cadastrar página no banco ...
+  await sonicChannelIngest.push(
+    "pages",
+    "default",
+    `page:${id}`,
+    `${title} ${content}`,
+    {
+      lang: "por",
+    }
+  );
   return res.status(202).send();
 });
 
