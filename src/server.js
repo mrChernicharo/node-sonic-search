210761dba7cba622a8b717eee346ccd861320644
Felipe Chernicharo <string7dev@gmail.com>
2021-03-04 00:39:14 -0300

    tenho conexão mas não consigo criar os registros

--- src/server.js
+++ src/server.js
@@ -3,11 +3,12 @@ const { v4: uuid } = require("uuid");
 const { Ingest } = require("sonic-channel");
 
 const app = express();
+app.use(express.json());
 
 const sonicChannelIngest = new Ingest({
-  // host: "localhost", <- não funciona pois meu docker roda por default nesse TCP aqui 192.168.99.100
-  host: "192.168.99.100",
+  // host: "localhost", // <- não funciona pois meu docker roda por default nesse TCP aqui 192.168.99.100
   //port: 2376
+  host: "192.168.99.100",
   port: 1491,
   auth: "SecretPassword", // <- não adianta mudar lá no sonic.cfg, a senha tem q ser essa
 });
@@ -28,31 +29,40 @@ sonicChannelIngest.connect({
   },
 });
 
-app.post("/pages", async (req, res) => {
-  const { title, content } = req.body;
-  const id = uuid();
+app.post("/pages", (req, res) => {
+  console.log("cria registros");
+  try {
+    const { title, content } = req.body;
+    const id = uuid();
 
-  console.log(title, content, id);
+    console.log(req.body);
 
-  // cadastrar página no banco ...
-  await sonicChannelIngest.push(
-    "pages",
-    "default",
-    `page:${id}`,
-    `${title} ${content}`,
-    {
-      lang: "por",
-    }
-  );
-  return res.status(202).send();
+    // cadastrar página no banco ...
+    sonicChannelIngest
+      .push("default", "pages", `page:${id}`, `${title} ${content}`, {
+        lang: "por",
+      })
+      .finally((e) => console.log(e));
+
+    return res.status(201).send();
+    //
+  } catch (e) {
+    console.log(e);
+
+    return res.status(500).send();
+  }
 });
 
 app.get("/search", (req, res) => {
-  //busca registros
+  console.log("busca registros");
   return res.json([]);
 });
 
 app.get("/suggest", (req, res) => {
+  return res.json([]);
+});
+
+app.get("/pages", (req, res) => {
   return res.json([]);
 });
 
