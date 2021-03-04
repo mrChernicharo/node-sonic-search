c82fb29016ecf0cd757f5183cf5019bf2d21ba36
Felipe Chernicharo <string7dev@gmail.com>
2021-03-03 21:07:59 -0300

    mais rotas

--- src/server.js
+++ src/server.js
@@ -4,8 +4,16 @@ const app = express();
 
 app.post("/pages", (req, res) => {
   // Salvar no banco
-
   return res.status(202).send();
+});
+
+app.get("/search", (req, res) => {
+  //busca registros
+  return res.json([]);
+});
+
+app.get("/suggest", (req, res) => {
+  return res.json([]);
 });
 
 app.listen(3333);
