02c5e0e8b6b68fec248d2fca6e029ece7511f782
Felipe Chernicharo <string7dev@gmail.com>
2021-03-03 22:04:52 -0300

    sonic supostamente conectado

--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const { v4: uuid } = require("uuid");
+const { Ingest } = require("sonic-channel");
 
 const app = express();
 
+const sonicChannelIngest = new Ingest({
+  host: "localhost",
+  port: 1491,
+  auth: "superSecretPassword",
+});
+
+sonicChannelIngest.connect({
+  connected: () => {
+    console.log("conectou");
+  },
+});
+
 app.post("/pages", (req, res) => {
   const { title, content } = req.body;
+  const id = uuid();
+
+  // cadastrar página no banco ...
 
   return res.status(202).send();
 });
